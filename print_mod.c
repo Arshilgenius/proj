78184742f3988aaee1dd47ddaf0b040e48042878
Arshil Khan <Arshilgenius@gmail.com>
2023-08-24 00:42:16 +0530

    Add files via upload

--- print_mod.c
+++ print_mod.c
@@ -1,19 +1,9 @@
 #include "main.h"
 #include <unistd.h>
 
-/**
- * print_mod - print %.
- *
- * @args: arguments.
- *
- * Return: count of chars.
- */
 
-int print_mod(va_list args)
-{
+int print_mod(va_list args){
 	(void)args;
-
 	_putchar('%');
-
 	return (1);
 }
